0346d2bab1f285a1c35f8d9aee020d0937c77736
Umar Afzal <umar.afzal1503@gmail.com>
2026-07-09 22:44:43 +0500

    listing products on the web page

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,15 @@
-console.log('hello world');
+import { products } from "./product.js";
+
+const productsGrid = document.getElementById("products-grid");
+
+products.forEach(product => {
+    const productHTML = `
+    <div class="product-card">
+        <img src="${product.image}" alt="${product.name}" class="product-image">
+        <h3 class="product-name" >${product.name}</h3>
+        <p class="product-price">$${product.price}</p>
+    </div>
+    `;
+
+    productsGrid.innerHTML += productHTML;
+})
---
+++ scripts/product.js
@@ -0,0 +1,158 @@
+export const products = [
+    { 
+        id: 1, 
+        name: "Bags", 
+        price: "$49.99", 
+        image: "images/bag.webp" 
+    },
+    { 
+        id: 2, 
+        name: "Bed Sheet", 
+        price: "$199.99", 
+        image: "images/bed-sheet.jpg" 
+    },
+    { 
+        id: 3, 
+        name: "Brush", 
+        price: "$35.00", 
+        image: "images/brush.webp" 
+    },
+    { 
+        id: 5, 
+        name: "Doll Doll House", 
+        price: "$89.99", 
+        image: "images/DollDoll_House.jpg" 
+    },
+    { 
+        id: 6, 
+        name: "Dresses", 
+        price: "$49.99", 
+        image: "images/DQC_APR_TBYB_W_DRESSES.jpg" 
+    },
+    { 
+        id: 7, 
+        name: "Action Figure", 
+        price: "$24.99", 
+        image: "images/Fuji_BTF_Quad_Cards_1x_Action_Figure.jpg" 
+    },
+    { 
+        id: 8, 
+        name: "Make-up Kit", 
+        price: "$39.99", 
+        image: "images/Fuji_BTF_Quad_Cards_1x_Make-up.jpg" 
+    },
+    { 
+        id: 9, 
+        name: "Mirrors", 
+        price: "$15.99", 
+        image: "images/Fuji_BTF_Quad_Cards_1x_Mirrors.jpg" 
+    },
+    { 
+        id: 10, 
+        name: "Sponges", 
+        price: "$9.99", 
+        image: "images/Fuji_BTF_Quad_Cards_1x_Sponges.jpg" 
+    },
+    { 
+        id: 11, 
+        name: "Books", 
+        price: "$12.50", 
+        image: "images/Fuji_Defect_Reduction_1x_Books.jpg" 
+    },
+    { 
+        id: 12, 
+        name: "Clothing", 
+        price: "$29.99", 
+        image: "images/Fuji_Defect_Reduction_1x_Clothing.jpg" 
+    },
+    { 
+        id: 13, 
+        name: "Equipments", 
+        price: "$149.99", 
+        image: "images/Fuji_Defect_Reduction_1x_Equipments.jpg" 
+    },
+    { 
+        id: 14, 
+        name: "Fashion Items", 
+        price: "$55.00", 
+        image: "images/Fuji_Defect_Reduction_1x_Fashion..jpg" 
+    },
+    { 
+        id: 15, 
+        name: "Gaming Headset", 
+        price: "$79.99", 
+        image: "images/gaming_head_set.webp" 
+    },
+    { 
+        id: 16, 
+        name: "Gaming Console", 
+        price: "$399.99", 
+        image: "images/Fuji_Defect_Reduction_1x_Gaming.jpg" 
+    },
+    { 
+        id: 17, 
+        name: "Hard Drives", 
+        price: "$89.99", 
+        image: "images/Fuji_Defect_Reduction_1x_Hard_Drives.jpg" 
+    },
+    { 
+        id: 18, 
+        name: "Headphones", 
+        price: "$69.99", 
+        image: "images/Fuji_Defect_Reduction_1x_Headphones.jpg" 
+    },
+    { 
+        id: 19, 
+        name: "Laptop Core", 
+        price: "$799.99", 
+        image: "images/Fuji_Defect_Reduction_1x_Laptop.jpg" 
+    },
+    { 
+        id: 20, 
+        name: "PC Desktop", 
+        price: "$1099.99", 
+        image: "images/Fuji_Defect_Reduction_1x_PC.jpg" 
+    },
+    { 
+        id: 21, 
+        name: "Speakers", 
+        price: "$45.00", 
+        image: "images/Fuji_Defect_Reduction_1x_Speakers.jpg" 
+    },
+    { 
+        id: 22, 
+        name: "Fitness Trackers", 
+        price: "$59.99", 
+        image: "images/Fuji_Defect_Reduction_1x_Trackers.jpg" 
+    },
+    { 
+        id: 23, 
+        name: "Laptop Pro", 
+        price: "$89.99", 
+        image: "images/laptop.jpg" 
+    },
+    { 
+        id: 24, 
+        name: "Pants", 
+        price: "$34.99", 
+        image: "images/pants.jpg" 
+    },
+    { 
+        id: 25, 
+        name: "Sea Salt Spray", 
+        price: "$18.50", 
+        image: "images/sea-salt-spray.webp" 
+    },
+    { 
+        id: 26, 
+        name: "Women's Watches", 
+        price: "$125.00", 
+        image: "images/WomenWatches_1x._SY116_CB564394432_.jpg" 
+    },
+    { 
+        id: 27, 
+        name: "Wow Spray", 
+        price: "$22.00", 
+        image: "images/wow-spray.webp" 
+    }
+];
